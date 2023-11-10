9905216c9b40ff3086df9599feaaac4f74e9b590
perochorichard <iamthedevelopernow@gmail.com>
2023-11-10 11:16:50 -0500

    add yuv to rgb conversion - added yuv to rgb converter in colors utility class

--- OpenGL/ColorUtils.cpp
+++ OpenGL/ColorUtils.cpp
@@ -2,9 +2,18 @@
 
 glm::vec3 ColorUtils::RGBtoYUV(glm::vec3 rgb) {
 	glm::mat3 rgbToYuvMatrix = {
-		{0.299, 0.587, 0.114},
-		{-0.14713, -0.28886, 0.436},
-		{0.615, -0.51499, -0.10001}
+		{ 0.299,    0.587,    0.114},
+		{-0.14713, -0.28886,  0.436},
+		{ 0.615,   -0.51499, -0.10001}
 	};
 	return rgbToYuvMatrix * rgb;
+}
+
+glm::vec3 ColorUtils::YUVtoRGB(glm::vec3 yuv) {
+	glm::mat3 yuvToRgbMatrix = {
+		{1,  0,        1.13983},
+		{1, -0.39465, -0.58060},
+		{1,  2.03211,  0}
+	};
+	return yuvToRgbMatrix * yuv;
 }
--- OpenGL/ColorUtils.h
+++ OpenGL/ColorUtils.h
@@ -7,6 +7,7 @@ class ColorUtils
 {
 public:
 	static glm::vec3 RGBtoYUV(glm::vec3 rgb);
+	static glm::vec3 YUVtoRGB(glm::vec3 yuv);
 };
 
 #endif // !COLOR_UTILS_H
